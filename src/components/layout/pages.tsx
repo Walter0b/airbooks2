2df98c120ec1f552365359bd541139eb50630bf0
Walter0b <69851744+Walter0b@users.noreply.github.com>
2024-04-18 13:04:53 +0100

    fix: table scroll

--- src/components/layout/pages.tsx
+++ src/components/layout/pages.tsx
@@ -18,7 +18,7 @@ export default function Pages({ children }: Readonly<PageProps>) {
     return (
         <div className="flex h-full ">
             {
-                <div className={`flex-1 min-w-96 w-full`}>
+                <div className={`flex-1 min-w-96 w-full overflow-hidden`}>
                     <div className="flex h-16 md:h-20 items-center max-sm:gap-9 sm:justify-between rounded-sm border-y">
 
                         {Header}
