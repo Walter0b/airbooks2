9a5481069465243e103c1c99d4beb25a17cffaae
Walter0b <69851744+Walter0b@users.noreply.github.com>
2024-06-24 17:30:46 +0100

    feat: create dynamic compact page

---
+++ src/app/(dashboard)/core/[pages]/[...id]/page.tsx
@@ -0,0 +1,25 @@
+// app/[...slug].js
+'use client'
+
+import GenericCompactPage from '@/components/layout/generic-page-details'
+import { pagesConfig } from '../_page'
+
+export default function DynamicPage({ params }: Readonly<{ params: { pages: string } }>) {
+    console.log("🚀 ~ DynamicPage ~ params.slug:", params.pages)
+
+
+
+    const pageConfig = pagesConfig[params.pages]
+
+    if (!pageConfig) return <p>Page not found</p>
+
+    const { fetchQuery, columns, tableOptions } = pageConfig
+
+    return (
+        <GenericCompactPage
+            fetchQuery={fetchQuery}
+            columns={columns}
+            tableOptions={tableOptions}
+        />
+    )
+}
---
+++ src/components/layout/generic-page-details.tsx
@@ -0,0 +1,48 @@
+"use client"
+import Header from '@/components/table/table.page.header'
+import Pages from '@/components/layout/pages'
+import TableLoader from '@/components/table/loader'
+import useSingleState from '@/hooks/useSingleState'
+import { GenericTablePageType } from '@/utils/models/interface/table'
+import TravelerCompactList from '@/app/(dashboard)/core/travelers/compact-list/compact-list'
+import TravelersItemDetails from '@/app/(dashboard)/core/travelers/compact-list/compact-list.details'
+
+
+
+export default function GenericCompactPage({
+    fetchQuery,
+    columns,
+    tableOptions,
+
+}: Readonly<GenericTablePageType>) {
+    console.log("🚀 ~ tableOptions:", tableOptions)
+    const page = useSingleState(1)
+    const pageSize = useSingleState(10)
+
+    const { data: tableData } = fetchQuery({
+        page: page.value,
+        pageSize: pageSize.value,
+    })
+
+    if (tableData) {
+        return (
+            <div className='flex h-screen'>
+
+
+
+                <div className=" h-full w-1/3 overflow-hidden border-r border-gray-300" data-slot="body">
+                    <Header data-slot="header" dropdownOptions={tableOptions} />
+                    <TravelerCompactList
+                        data-slot="compactList"
+                        tableData={tableData}
+                    />
+
+                </div>
+                <TravelersItemDetails children={undefined} fetchQuery={undefined} />
+
+            </div>
+        )
+    } else {
+        return <TableLoader />
+    }
+}
