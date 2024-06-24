e4665fe92b990180e442afc20d6667f727ee4404
Walter0b <69851744+Walter0b@users.noreply.github.com>
2024-06-24 17:28:49 +0100

    feat: add custom className and navlink manager

---
+++ src/app/(dashboard)/core/[pages]/_page.ts
@@ -0,0 +1,20 @@
+import { useFetchCustomersQuery, useFetchTravelersQuery } from "@/states/reducer/apiSlice";
+import { travelerTableOptions } from "@/static/travelers/table/dropdown";
+import { PageConfigType } from "@/utils/models/interface/table";
+import { travelersColumns } from "../travelers/table";
+import { customerColumns } from "../customers/table";
+import { customerTableOptions } from "../customers/dropdown";
+
+export const pagesConfig: PageConfigType = {
+    travelers: {
+        fetchQuery: useFetchTravelersQuery,
+        columns: travelersColumns,
+        tableOptions: travelerTableOptions,
+    },
+
+    customer: {
+        fetchQuery: useFetchCustomersQuery,
+        columns: customerColumns,
+        tableOptions: customerTableOptions,
+    }
+}
