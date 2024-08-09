cccb00effc6ada686be9dd3d6db84e9ebdf6bed8
Walter0b <69851744+Walter0b@users.noreply.github.com>
2024-08-09 16:36:03 +0100

    feat: add dynamic api calls

---
+++ src/states/reducer/apiSlice.endpoint.ts
@@ -0,0 +1,27 @@
+
+interface EndpointConfig {
+    name: string;
+    endpoint: string;
+}
+
+
+export const endpointConfig: EndpointConfig[] = [
+    { name: 'Travelers', endpoint: 'traveler' },
+    { name: 'Customers', endpoint: 'customer' },
+    { name: 'AgencyProfile', endpoint: 'agency' },
+    { name: 'Users', endpoint: 'user' },
+    { name: 'ChartsOfAccounts', endpoint: 'chart_of_accounts' },
+    { name: 'Products', endpoint: 'products' },
+    { name: 'Currencies', endpoint: 'currency' },
+    { name: 'BusinessRules', endpoint: 'business_rules' },
+    { name: 'Preferences', endpoint: 'preferences' },
+    { name: 'Integration', endpoint: 'integration' },
+    { name: 'TravelItem', endpoint: 'travel_item' },
+    { name: 'Estimates', endpoint: 'estimates' },
+    { name: 'Invoice', endpoint: 'invoice' },
+    { name: 'CreditNotes', endpoint: 'credit_notes' },
+    { name: 'Payments', endpoint: 'payments' },
+    { name: 'Bill', endpoint: 'bill' },
+    { name: 'Expenses', endpoint: 'expenses' },
+    { name: 'ManualJournal', endpoint: 'manual_journal' },
+]
--- src/states/reducer/apiSlice.ts
+++ src/states/reducer/apiSlice.ts
@@ -1,101 +1,134 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import {
-    ResponseDataType,
-    TableDataType,
-} from '@/utils/types/page-type/table.type'
-import { getSession } from 'next-auth/react'
+    createApi,
+    fetchBaseQuery,
+    FetchBaseQueryError,
+    BaseQueryFn,
+    FetchArgs,
+    FetchBaseQueryMeta,
+    EndpointBuilder
+} from '@reduxjs/toolkit/query/react';
+import { ResponseDataType, TableDataType } from '@/utils/types/page-type/table.type';
+import { getSession } from 'next-auth/react';
+import { endpointConfig } from './apiSlice.endpoint';
 
-const apiUrl = process.env.NEXT_PUBLIC_BASE_URL
+const apiUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: apiUrl + '/items',
+    baseUrl: `${apiUrl}/items`,
     prepareHeaders: async (headers) => {
-        const session = await getSession()
-        const token = session?.user.accessToken
-        const expiresAt = session?.user.expiresAt
+        const session = await getSession();
+        const token = session?.user.accessToken;
+        const expiresAt = session?.user.expiresAt;
 
         if (token && expiresAt && new Date().getTime() < expiresAt) {
-            headers.set('Authorization', `Bearer ${token}`)
+            headers.set('Authorization', `Bearer ${token}`);
         }
 
-        headers.set('Content-Type', 'application/json')
-        return headers
+        headers.set('Content-Type', 'application/json');
+        return headers;
     },
-
-    paramsSerializer: (params) => {
+    paramsSerializer: (params: Record<string, any>) => {
         const queryString = new URLSearchParams(
             Object.entries(params).filter(([_, value]) => value !== undefined)
-        ).toString()
-        return `${queryString}&meta=*`
+        ).toString();
+        return `${queryString}&meta=*`;
     },
-})
+});
+
+interface EndpointConfig {
+    name: string;
+    endpoint: string;
+}
+
+
+interface QueryParams {
+    page: number;
+    pageSize: number;
+    fields?: string;
+    filter?: Record<string, any>;
+    search?: string;
+    sort?: string;
+}
+
+type CustomBaseQuery = BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError, {}, FetchBaseQueryMeta>;
+
+const generateEndpoints = (
+    builder: EndpointBuilder<CustomBaseQuery, string, string>
+) => {
+    const endpoints: Record<string, any> = {};
+
+    endpointConfig.forEach(({ name, endpoint }) => {
+        endpoints[`fetch${name}`] = builder.query<ResponseDataType, QueryParams>({
+            query: ({ page, pageSize, fields, filter, search, sort }) => ({
+                url: endpoint,
+                params: {
+                    page,
+                    limit: pageSize,
+                    ...(fields && { fields }),
+                    ...(filter && { filter }),
+                    ...(search && { search }),
+                    ...(sort && { sort }),
+                },
+            }),
+            providesTags: [name],
+        });
+
+        endpoints[`create${name}`] = builder.mutation<ResponseDataType, TableDataType>({
+            query: (body) => ({
+                url: endpoint,
+                method: 'POST',
+                body,
+            }),
+            invalidatesTags: [name],
+        });
+    });
+
+    return endpoints;
+};
 
 export const api = createApi({
     reducerPath: 'api',
     baseQuery,
-    tagTypes: ['Travelers', 'Customers'],
-    endpoints: (builder) => ({
-        fetchTravelers: builder.query<
-            ResponseDataType,
-            {
-                page: number
-                pageSize: number
-                fields?: string
-                filter?: any
-                search?: string
-                sort?: string
-            }
-        >({
-            query: ({ page, pageSize, fields, filter, search, sort }) => ({
-                url: 'traveler',
-                params: {
-                    page,
-                    limit: pageSize,
-                    ...(fields && { fields }),
-                    ...(filter && { filter }),
-                    ...(search && { search }),
-                    ...(sort && { sort }),
-                },
-            }),
-            providesTags: ['Travelers'],
-        }),
-        fetchCustomers: builder.query<
-            ResponseDataType,
-            {
-                page: number
-                pageSize: number
-                fields?: string
-                filter?: any
-                search?: string
-                sort?: string
-            }
-        >({
-            query: ({ page, pageSize, fields, filter, search, sort }) => ({
-                url: 'customer',
-                params: {
-                    page,
-                    limit: pageSize,
-                    ...(fields && { fields }),
-                    ...(filter && { filter }),
-                    ...(search && { search }),
-                    ...(sort && { sort }),
-                },
-            }),
-            providesTags: ['Customers'],
-        }),
-        createTraveler: builder.mutation<ResponseDataType, TableDataType>({
-            query: (body) => ({
-                url: 'traveler',
-                method: 'POST',
-                body,
-            }),
-            invalidatesTags: ['Travelers'],
-        }),
-    }),
-})
+    tagTypes: endpointConfig.map(({ name }) => name),
+    endpoints: generateEndpoints,
+});
+
 
 export const {
     useFetchTravelersQuery,
     useFetchCustomersQuery,
+    useFetchAgencyProfileQuery,
+    useFetchUsersQuery,
+    useFetchChartsOfAccountsQuery,
+    useFetchProductsQuery,
+    useFetchCurrenciesQuery,
+    useFetchBusinessRulesQuery,
+    useFetchPreferencesQuery,
+    useFetchIntegrationQuery,
+    useFetchTravelItemQuery,
+    useFetchEstimatesQuery,
+    useFetchInvoiceQuery,
+    useFetchCreditNotesQuery,
+    useFetchPaymentsQuery,
+    useFetchBillQuery,
+    useFetchExpensesQuery,
+    useFetchManualJournalQuery,
     useCreateTravelerMutation,
-} = api
+    useCreateCustomerMutation,
+    useCreateAgencyProfileMutation,
+    useCreateUserMutation,
+    useCreateChartsOfAccountsMutation,
+    useCreateProductMutation,
+    useCreateCurrencyMutation,
+    useCreateBusinessRuleMutation,
+    useCreatePreferenceMutation,
+    useCreateIntegrationMutation,
+    useCreateTravelItemMutation,
+    useCreateEstimateMutation,
+    useCreateInvoiceMutation,
+    useCreateCreditNoteMutation,
+    useCreatePaymentMutation,
+    useCreateBillMutation,
+    useCreateExpenseMutation,
+    useCreateManualJournalMutation,
+} = api;
